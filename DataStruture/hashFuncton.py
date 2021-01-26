e510c3676f007596ffd2c3aa9c179be78961bceb
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2021-01-26 12:18:16 +0530

    master commit on jan26 2021

--- DataStruture/hashFuncton.py
+++ DataStruture/hashFuncton.py
@@ -1,7 +1,7 @@
 class HashTable:
     def __init__(self):
         self.Max=10
-        self.array=[0 for i in range(10)]
+        self.array=[0 for i in range(100)]
 
     def getHash(self,element):
         h=0#For adding the ordered value
@@ -9,13 +9,22 @@ class HashTable:
             h+=ord(i)#for calculating the ASCII Value
         place=h%self.Max #10 is size of memory location
         return place
-    def push(self,key,value):
+    def __setitem__(self,key,value): # insted of this push(self,key,value)
         hash=self.getHash(key)
-
         self.array[hash]=value
+    def __getitem__(self,key):# insted of this get(self,key):
+        hash=self.getHash(key)
+        return self.array[hash]
+    def __delitem__(self,key):
+        hash=self.getHash(key)
+        self.array[hash]=0
 
 hash=HashTable()
-hash.push("march 6",99)
+hash["march 6"]=99 #instead of this hash.push("march 6",99)
+
+hash["march 18"]="March 17"
+del hash["march 18"]
 print(hash.array)
+
 
     
