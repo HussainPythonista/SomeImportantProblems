a91a7d10713131b5c032b49de45cb4f3e9c6370a
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2021-01-26 09:55:24 +0530

    only add get function

--- DataStruture/hashFuncton.py
+++ DataStruture/hashFuncton.py
@@ -3,14 +3,19 @@ class HashTable:
         self.Max=10
         self.array=[0 for i in range(10)]
 
-    def getHash(self,date):
+    def getHash(self,element):
         h=0#For adding the ordered value
-        for i in date:
+        for i in element:
             h+=ord(i)#for calculating the ASCII Value
         place=h%self.Max #10 is size of memory location
-        self.array[place]=99
+        return place
+    def push(self,key,value):
+        hash=self.getHash(key)
+
+        self.array[hash]=value
+
 hash=HashTable()
-hash.getHash("march 6")
+hash.push("march 6",99)
 print(hash.array)
 
     
