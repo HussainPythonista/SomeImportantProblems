7f64ee822e4b1a82e1f69f8ea59ab46b304e5c20
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2021-01-27 21:57:17 +0530

    tree implimenations

--- DataStruture/tree.py
+++ DataStruture/tree.py
@@ -1,7 +1,33 @@
-class Node:
-    def __init__(self,leftRef=None,data=None,rightRef=None):
-        self.leftRef=leftRef
+class treeNode:
+    def __init__(self,data=None):
+        self.parents=None
         self.data=data
-        self.rightRef=rightRef
-class Tree:
+        self.children=[]
+
+    def addChild(self,data):
+        self.parents=self
+        self.children.append(data)
+    def printDatas(self):
+        if len(self.children)>1:
+            for child in self.children:
+                print(child.data)
+def treeDataStructure():
+    root=treeNode("Electronics")
+    laptop=treeNode("Laptop")
+    laptop.addChild("Lenovo")
+    laptop.addChild("MacBook")
+    laptop.addChild("MicroSoft Surface")
+    mobiles=treeNode("Mobiles")
+    mobiles.addChild("OnePlus")
+    mobiles.addChild("Iphone")
+    mobiles.addChild("Mi")
+    condoms=treeNode("Condoms")
+    condoms.addChild("Manforce")
+    condoms.addChild("Moods")
+    condoms.addChild("Durex")
     
+    root.addChild(laptop)
+    root.addChild(mobiles)
+    root.addChild(condoms)
+    
+treeDataStructure()
