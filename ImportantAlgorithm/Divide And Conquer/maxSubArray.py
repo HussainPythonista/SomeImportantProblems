cfb7b160cec22dbddbf90e1f718ef8b34147d557
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2021-03-02 10:09:47 +0530

    kadane's Algorithm

--- ImportantAlgorithm/Divide And Conquer/maxSubArray.py
+++ ImportantAlgorithm/Divide And Conquer/maxSubArray.py
@@ -1,18 +1,25 @@
-def naiveApproach(listValue):
-    maxValue=0
-    for i in range(len(listValue)):
-        for j in range(i+1,len(listValue)):
-            listValue[i]+=listValue[j]
-            maxValue=max(listValue[i],maxValue)
-    print(maxValue)
-
-def maxSubArray(listValue):#Optimal Solution O(n)
-    maxValue=0
-    current=0
-    for i in range(1,len(listValue)):
-        current=max(listValue[i],current+listValue[i])
-        maxValue=max(current,maxValue)
-
+def NaiveApproach(array):#this is Big O (n)**2
+    maxValue=-999999999999999999999999999999999999999999999999
+    for i in range(len(array)-1):
+        for j in range(i+1,len(array)):
+            array[i]+=array[j]
+            maxValue=max(maxValue,array[i])
     return maxValue
-listValue=[-2,1,-3,4,-1,2,1,-5,4]
-print(maxSubArray(listValue))
+array=[-19,1,2,70,3,-4,10]
+
+def LinearTiming(array):#This Is Kadane's Algorithm
+    currentSum=-99999999999999999999999999999999999999999999
+    maxValue=currentSum
+    for i in range(len(array)):
+        currentSum=max(array[i],array[i]+currentSum)
+        maxValue=max(maxValue,currentSum)
+    return maxValue
+def anothemethod(array):
+    currentSum=array[len(array)-1]
+    maxValue=currentSum
+    for i in range(len(array)-2,0,-1):
+        currentSum=max(array[i],array[i]+currentSum)
+        print(currentSum)
+        maxValue=max(currentSum,maxValue)
+        
+    return maxValue
